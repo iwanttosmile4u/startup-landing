f7eb7c5ad98063de4509f92d8aff98a2d0258f94
Tetiana Panasiuke <tetiana.panasiuk21@gmail.com>
2022-01-27 00:06:26 +0200

    done header with navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,30 @@
-const navToggle = document.querySelector('.header__menu-btn');
-const navList = document.querySelector('.navbar');
+const menuBtn = document.querySelector('.header__menu-btn');
+const burgerItem = document.querySelector('.header__burger-btn');
+let activeBtn = false;
 
-navToggle.addEventListener('click', () => {
-  navList.classList.toggle('nav-open');
+// Hamburger menu - changing button
+
+menuBtn.addEventListener('click', () => {
+  if (!activeBtn) {
+    menuBtn.classList.add('burger-open');
+    burgerItem.classList.add('burger-open__burger-btn');
+    activeBtn = true;
+  } else {
+    menuBtn.classList.remove('burger-open');
+    burgerItem.classList.remove('burger-open__burger-btn');
+    activeBtn = false;
+  }
 });
+
+// Showing menu - mobile and tablet
+
+const showMenu = (btn, menuList) => {
+  const toggleBtn = document.querySelector(btn);
+  const menu = document.querySelector(menuList);
+  if (toggleBtn && menu) {
+    toggleBtn.addEventListener('click', () => {
+      menu.classList.toggle('navigation__open');
+    });
+  }
+};
+showMenu('#burger', '#nav');
